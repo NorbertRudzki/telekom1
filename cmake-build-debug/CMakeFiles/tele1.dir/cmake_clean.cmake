45b974cdb4cf070613788362a9a70451ab5fba69
Norbert <norudzki@gmail.com>
2020-03-10 20:03:13 +0100

    import

---
+++ cmake-build-debug/CMakeFiles/tele1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tele1.dir/main.cpp.obj"
+  "libtele1.dll.a"
+  "tele1.exe"
+  "tele1.exe.manifest"
+  "tele1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tele1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
